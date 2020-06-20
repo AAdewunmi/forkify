c3305624906b975e88fb554b0bd51679dffedf5f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-06-20 09:13:17 +0100

    Fifth Commit: Set up Babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 // Global app controller
+
 import num from './test';
-console.log(`I imported ${num} from another module called test.js!`);
+var x = 23;
+console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -15,5 +15,12 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    }
 };
