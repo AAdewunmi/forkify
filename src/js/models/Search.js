dc2d01227a9f0c8a5009331a273f2b484ce118af
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-06-24 16:32:13 +0100

    Nineth Commit: Build Search Controller (MVC)

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,6 @@ export default class Search{
             const res = await axios(
                 `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
             );
-            
             this.result = res.data.recipes;
             //console.log(this.result);
         } catch (error) {
