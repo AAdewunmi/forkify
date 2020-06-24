5943ec3fa2466254d9bf0f62352deea3233446f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-06-24 14:17:44 +0100

    Eighth Commit: Build Search Model (MVC)

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
 // Global app controller
-
-    import axios from "axios";
-
-    async function getResults(query) {
-        try {
-            const res = await axios(
-                `https://forkify-api.herokuapp.com/api/search?&q=${query}`
-            );
-            const recipes = res.data.recipes;
-            console.log(recipes);
-        } catch (error) {
-            alert(error);
-        }
-    }
-    getResults('radish');
+import Search from './models/Search';
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
