97d949b1fb04fdff047285a3318a80c58cd1beb3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-07-12 14:44:12 +0100

    Tenth Commit: rending an AJAX loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base'; 
+import { elements, renderLoader, clearLoader } from './views/base'; 
 
 /** Global state of the app
  * - Search object
@@ -22,10 +22,12 @@ const controlSearch = async () => {
     // 3) Prepare UI for results 
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
     }
 };
