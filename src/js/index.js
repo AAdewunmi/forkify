13ed289ad21f6a1c92d7b545388cd206116beeb0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-07-23 14:39:06 +0100

    Sixteenth Commit: Building the Shopping List Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 
@@ -65,7 +66,7 @@ const controlRecipe = async () => {
         
         try{
             await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
+            //console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
             state.recipe.calcTime();
             state.recipe.calcServings();
@@ -91,5 +92,7 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
+    //console.log(state.recipe);
 });
+
+window.l = new List();
